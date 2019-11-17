a02b97ebe154c2b78949b7e3176ba47409df9011
ShotaTogawa <s.togawa0112@gmail.com>
2019-11-16 16:57:37 -0800

    made user schema

---
+++ src/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      }
+    },
+    hashed_password: {
+      type: String,
+      required: true
+    },
+    salt: String,
+    avatar: {
+      type: Buffer,
+      default: null
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
