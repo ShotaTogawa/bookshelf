0043d77f669cc2084173971f5e3fd5ba2176ab7a
ShotaTogawa <s.togawa0112@gmail.com>
2019-11-19 19:44:16 -0800

    made test cases for auth

---
+++ tests/auth.test.js
@@ -0,0 +1,17 @@
+const request = require("supertest");
+const app = require("../src/app");
+const User = require("../src/models/user");
+
+test("should signup a user", async () => {});
+
+test("should not signup a user", async () => {});
+
+test("should signin a user", async () => {});
+
+test("should not signin a user", async () => {});
+
+test("should signout a user", async () => {});
+
+test("a user should be authenticated", async () => {});
+
+test("a user should not be authenticated", async () => {});
