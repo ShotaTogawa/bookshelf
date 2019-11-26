2abe9725cd021d8e9bfc5ee6f2be91f50983e14d
ShotaTogawa <s.togawa0112@gmail.com>
2019-11-26 09:32:06 -0800

    made basic memo router

--- src/router/memo.js
+++ src/router/memo.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/books/:userId/:bookId/memo");
-router.post("/books/:userId/:bookId/memo");
-router.get("/books/:userId/:bookId/memo/:memoId");
-router.put("/books/:userId/:bookId/memo/:memoId");
-router.delete("/books/:userId/:bookId/memo/:memoId");
+const { requireSignin, isAuth } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
+const { bookById } = require("../controllers/book");
+const {
+  getMemos,
+  takeMemo,
+  getMemo,
+  updateMemo,
+  deleteMemo
+} = require("../controllers/memo");
+
+router.get("/books/:userId/:bookId/memo", requireSignin, isAuth, getMemos);
+router.post("/books/:userId/:bookId/memo", requireSignin, isAuth, takeMemo);
+router.get("/books/:userId/:bookId/memo/:memoId", getMemo);
+router.put("/books/:userId/:bookId/memo/:memoId", updateMemo);
+router.delete("/books/:userId/:bookId/memo/:memoId", deleteMemo);
+
+router.param("userId", userById);
+router.param("bookId", bookById);
 
 module.exports = router;
