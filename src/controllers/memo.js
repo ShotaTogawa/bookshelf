9edbfd8d6df8fac5987be0895e60d45bc8e089ad
ShotaTogawa <s.togawa0112@gmail.com>
2019-11-26 09:35:08 -0800

    put templates in  memo controller

---
+++ src/controllers/memo.js
@@ -0,0 +1,7 @@
+const Memo = require("../models/memo");
+
+exports.getMemos = async (req, res) => {};
+exports.takeMemo = async (req, res) => {};
+exports.getMemo = async (req, res) => {};
+exports.updateMemo = async (req, res) => {};
+exports.deleteMemo = async (req, res) => {};
