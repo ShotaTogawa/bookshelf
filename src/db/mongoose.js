64e9ed7106253a13cdaef568f906769e1bd0f80b
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-20 16:41:38 -0800

    added async await for connect db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,15 +1,33 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  })
-  .then(() => {
-    console.log("connected");
-  })
-  .catch(e => console.log("DB error", e));
+// mongoose
+//   .connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+//   })
+//   .then(() => {
+//     console.log("connected");
+//   })
+//   .catch(e => console.log("DB error", e));
 
-module.exports = mongoose;
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+// module.exports = mongoose;
+module.exports = connectDB;
